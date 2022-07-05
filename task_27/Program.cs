35c19bff8ea4f5b2266ec112d2d4b687b1b4d5a6
SfShev <Pshev1406@gmail.com>
2022-07-06 00:41:25 +0300

    Сумма цифр числа

---
+++ task_27/Program.cs
@@ -0,0 +1,33 @@
+﻿int SumOfDigits(int Number)
+{
+    int remainder = 0;
+    int summ = 0;
+    for (; Number >= 1; summ += remainder)
+    {
+        remainder = Number % 10;
+        Number = Number / 10;
+    }
+    for (; Number <= -1; summ += remainder)
+    {
+        remainder = Number % 10;
+        Number = Number / 10;
+    }
+    return Math.Abs(summ);
+}
+Console.Write("Введите число у которого хотите узнать сумму его цифр: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр = {SumOfDigits(num)}");
+
+/*
+}
+    int i = 0;
+    int a = 0;
+    while (Number >= 1)
+    {
+        a = Number % 10;
+        Number = Number / 10;
+        i = i + a ;
+    }
+    return i;
+}
+*/
